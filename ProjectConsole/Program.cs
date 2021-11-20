1c1dce4be02b5e49f6702ea5120cf93be9e43b54
Gabriel <gabriel.sh010204@gmail.com>
2021-11-20 00:28:04 -0300

    ini

---
+++ ProjectConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<int> firstGroup = new List<int>();
+List<int> secondGroup = new List<int>();
+
+Console.WriteLine("Digite 12 números entre 1 e 20: ");
+
+for (int x = 0; x <= 12; x++)
+{
+    String input = Console.ReadLine();
+    int number = Int32.Parse(input);
+
+    if (number <= 10 && number % 2 == 0)
+    {
+        firstGroup.Add(number);
+    }
+    if (number > 10)
+    {
+        secondGroup.Add(number);
+    }
+}
+
+string add1 = "";
+firstGroup.ForEach(n => add1 += n + " ");
+Console.WriteLine("Lista 1: " + add1);
+
+string add2 = "";
+secondGroup.ForEach(n => add2 += n + " ");
+Console.WriteLine("Lista 2: " + add2);
